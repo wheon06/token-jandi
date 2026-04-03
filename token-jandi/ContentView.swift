33964c9075569f31515c0bc0721d71ba2e3d373a
이희연 HeeYeon Lee <dgc394722@gmail.com>
2026-04-03 10:00:33 +0900

    Initial Commit

---
+++ token-jandi/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  token-jandi
+//
+//  Created by 이희연 on 4/3/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ token-jandi/token_jandiApp.swift
@@ -0,0 +1,17 @@
+//
+//  token_jandiApp.swift
+//  token-jandi
+//
+//  Created by 이희연 on 4/3/26.
+//
+
+import SwiftUI
+
+@main
+struct token_jandiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
